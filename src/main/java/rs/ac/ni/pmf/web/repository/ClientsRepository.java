0dcf8b67157b7e60dc3a4d253d312ab94f11eea4
Danilo Petkovic <petkovicdanilo97@gmail.com>
2020-06-11 00:29:21 +0200

    Override findAll to return List in repositories

--- src/main/java/rs/ac/ni/pmf/web/repository/ClientsRepository.java
+++ src/main/java/rs/ac/ni/pmf/web/repository/ClientsRepository.java
@@ -1,9 +1,13 @@
 package rs.ac.ni.pmf.web.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import rs.ac.ni.pmf.web.model.entity.ClientEntity;
 
 public interface ClientsRepository extends PagingAndSortingRepository<ClientEntity, Integer>{
 
+	@Override
+	List<ClientEntity> findAll();
 }
--- src/main/java/rs/ac/ni/pmf/web/repository/WorkersRepository.java
+++ src/main/java/rs/ac/ni/pmf/web/repository/WorkersRepository.java
@@ -1,9 +1,13 @@
 package rs.ac.ni.pmf.web.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import rs.ac.ni.pmf.web.model.entity.WorkerEntity;
 
 public interface WorkersRepository extends PagingAndSortingRepository<WorkerEntity, String> {
 
+	@Override
+	List<WorkerEntity> findAll();
 }
