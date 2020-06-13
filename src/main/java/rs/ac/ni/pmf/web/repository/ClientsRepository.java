4a6dd9611fb2f16d76b8a36e2b3d6f46a7ec3eb9
Danilo Petkovic <petkovicdanilo97@gmail.com>
2020-06-13 19:37:06 +0200

    Add repository annotation

--- src/main/java/rs/ac/ni/pmf/web/repository/ClientsRepository.java
+++ src/main/java/rs/ac/ni/pmf/web/repository/ClientsRepository.java
@@ -3,9 +3,11 @@ package rs.ac.ni.pmf.web.repository;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import rs.ac.ni.pmf.web.model.entity.ClientEntity;
 
+@Repository
 public interface ClientsRepository extends PagingAndSortingRepository<ClientEntity, Integer>{
 
 	@Override
--- src/main/java/rs/ac/ni/pmf/web/repository/PartsRepository.java
+++ src/main/java/rs/ac/ni/pmf/web/repository/PartsRepository.java
@@ -3,9 +3,11 @@ package rs.ac.ni.pmf.web.repository;
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import rs.ac.ni.pmf.web.model.entity.PartEntity;
 
+@Repository
 public interface PartsRepository extends PagingAndSortingRepository<PartEntity, Integer> {
 
 	@Override
