0049d66a977318dd26e277fdbb6445aa24d4476f
Danilo Petkovic <petkovicdanilo97@gmail.com>
2020-06-15 18:03:51 +0200

    Add info about api to swagger

--- src/main/java/rs/ac/ni/pmf/web/config/SwaggerConfig.java
+++ src/main/java/rs/ac/ni/pmf/web/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,9 +26,22 @@ public class SwaggerConfig implements WebMvcConfigurer
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build()
-				.pathMapping("/services/rest");
+				.pathMapping("/services/rest")
+				.apiInfo(createApiInfo());
 	}
 
+	private ApiInfo createApiInfo() {
+		return new ApiInfoBuilder()
+				.title("Artisan API")
+				.description("REST API for artisan company")
+				.contact(new Contact(
+						"Danilo Petkovic", 
+						"https://github.com/petkovicdanilo/artisan-api", 
+						"danilo.petkovic@pmf.edu.rs"))
+				.version("v1")
+				.build();
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry)
 	{
