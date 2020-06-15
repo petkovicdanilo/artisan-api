06dc995b1f840dd9f213d6d0b248dec633cff6b3
Danilo Petkovic <petkovicdanilo97@gmail.com>
2020-06-15 20:07:05 +0200

    Add api documentation to WorkerDTO

--- src/main/java/rs/ac/ni/pmf/web/model/api/WorkerDTO.java
+++ src/main/java/rs/ac/ni/pmf/web/model/api/WorkerDTO.java
@@ -1,6 +1,7 @@
 package rs.ac.ni.pmf.web.model.api;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,10 +14,24 @@ import lombok.Value;
 @Builder
 @ApiModel(description = "Data object representing worker")
 public class WorkerDTO {
+	
+	@ApiModelProperty(value = "Username of worker. Must be unique.", example = "worker1")
 	private String username;
+	
+	@ApiModelProperty(value = "Is worker admin", example = "false")
 	private boolean isAdmin;
+	
+	@ApiModelProperty(
+		value = "Worker's email. Must be unique.", 
+		example = "worker1@artisan-api.com")
 	private String email;
+	
+	@ApiModelProperty(value = "Worker's first name", example = "John")
 	private String firstName;
+	
+	@ApiModelProperty(value = "Worker's last name", example = "Doe")
 	private String lastName;
+	
+	@ApiModelProperty(name = "Worker's phone number", example = "+381-18-123-456")
 	private String phoneNumber;
 }
